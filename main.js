112bbbe03a6d22b69300324989a5c31b42c4bd56
Justo6 <mikejusto6@gmail.com>
2016-12-12 14:43:18 -0800

    for loop showing 10 photos

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
 /* Sean, Miles, Mike, Vernon    Music Venue     Hack-a-thon     December 12-13, 2016    */
 
 
-
-
 var zipcode;
+var imageSearch;
 
 $(document).ready(function(){
 
@@ -24,4 +23,29 @@ $(document).ready(function(){
         });
         console.log('End of click function');
     });
+    // flicker API call begins here
+    $('button').click(function(){
+        imageSearch = $("#imageSearch").val();
+        console.log('click initiated');
+        $.ajax({
+            dataType: 'json',
+            url: "https://api.flickr.com/services/rest?method=flickr.photos.search&api_key=4291af049e7b51ff411bc39565109ce6&format=json&nojsoncallback=1&text=" + imageSearch,
+            success: function(result) {
+                console.log(result.photos.photo[0].server);
+                console.log(result.photos.photo[0].id);
+                console.log(result.photos.photo[0].secret);
+
+                var server = result.photos.photo[0].server;
+                var photoId = result.photos.photo[0].id;
+                var secret = result.photos.photo[0].secret;
+                var image =  $("<img>").attr("src", "https://farm1.staticflickr.com/"+server+"/"+photoId+ "_"+secret +".jpg");
+
+                $('body').append(image);
+
+
+            }
+        });
+        console.log('End of click function');
+    });
+
 });
